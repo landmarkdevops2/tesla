1fedac16eb867af298ea4bf70370683724ef4efe
simon <simon@yahoo.com>
2022-05-22 17:32:20 +0000

    ignored files

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,18 @@
 deploy java application
 deploy python apps
 deploy in the dev env
+<<<<<<< HEAD
 deployment5
 deployment4
 deploy to staging 
 echo Good Job
 tested and accepted
 # this is where i made that error
+=======
+deployment5 master
+deployment4
+
+deploy to staging env
+echo good job
+tested and accepted
+>>>>>>> ee1151a (deploy to stagine environment updated)
