db991d168530996c27a260217d20341db3775057
simon <simon@yahoo.com>
2022-05-22 13:30:50 +0000

    move to deployment4

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@ deploy python apps
 deploy in the dev env
 deployment5 master
 deployment4
+<<<<<<< HEAD
 
 deploy to staging env
 echo good job
 tested and accepted
+=======
+deploy to staging
+>>>>>>> abb3376 (deploy to the staging environment)
