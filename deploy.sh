6153d8a778e19f2cafbe59a3d423e4f59371e848
simon <simon@yahoo.com>
2022-05-22 17:32:20 +0000

    deploy to stagine environment updated

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 deploy java application
 deploy python apps
 deploy in the dev env
+<<<<<<< HEAD
 deployment5 master
+=======
+<<<<<<< HEAD
+deployment5
+>>>>>>> 1fedac1 (ignored files)
 deployment4
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -9,9 +14,21 @@ deployment4
 deploy to staging env
 echo good job
 tested and accepted
+<<<<<<< HEAD
 =======
 deploy to staging
 >>>>>>> abb3376 (deploy to the staging environment)
 =======
 deploy to staging
 >>>>>>> abb3376 (deploy to the staging environment)
+=======
+# this is where i made that error
+=======
+deployment5 master
+deployment4
+
+deploy to staging env
+echo good job
+tested and accepted
+>>>>>>> ee1151a (deploy to stagine environment updated)
+>>>>>>> 1fedac1 (ignored files)
