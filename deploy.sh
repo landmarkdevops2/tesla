65a248acf816890e10dba880f38e69e34cecbb57
simon <simon@yahoo.com>
2022-05-22 15:34:46 +0000

    updated with new message

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 deploy java application
 deploy python apps
 deploy in the dev env
-
+deployment5
 deployment4
 deploy to staging 
 echo Good Job
 tested and accepted
+# this is where i made that error
