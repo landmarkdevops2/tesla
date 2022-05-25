2e3d6e3117df74dab0638774bb941c9b09fae332
des1122 <105686179+des1122@users.noreply.github.com>
2022-05-24 22:12:13 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 deploy java application
 deploy python apps
 deploy in the dev env
-<<<<<<< HEAD
 deployment5
 deployment4
 deploy to staging 
@@ -15,4 +14,4 @@ deployment4
 deploy to staging env
 echo good job
 tested and accepted
->>>>>>> ee1151a (deploy to stagine environment updated)
+ee1151a (deploy to stagine environment updated)
