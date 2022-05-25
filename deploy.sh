7eff4cbdf450ceda79da02f0fb64f2516ae77103
des1122 <105686179+des1122@users.noreply.github.com>
2022-05-24 22:11:32 -0700

    changes done

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,23 @@
 deploy java application
 deploy python apps
 deploy in the dev env
-<<<<<<< HEAD
 deployment5 master
 =======
-<<<<<<< HEAD
 deployment5
->>>>>>> 1fedac1 (ignored files)
+1fedac1 (ignored files)
 deployment4
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 =======
->>>>>>> db991d1 (move to deployment4)
+db991d1 (move to deployment4)
 
 deploy to staging env
 echo good job
 tested and accepted
-<<<<<<< HEAD
-<<<<<<< HEAD
 =======
 deploy to staging
->>>>>>> abb3376 (deploy to the staging environment)
+abb3376 (deploy to the staging environment)
 =======
 deploy to staging
->>>>>>> abb3376 (deploy to the staging environment)
+abb3376 (deploy to the staging environment)
 =======
 # this is where i made that error
 =======
@@ -34,10 +27,10 @@ deployment4
 deploy to staging env
 echo good job
 tested and accepted
->>>>>>> ee1151a (deploy to stagine environment updated)
->>>>>>> 1fedac1 (ignored files)
+ee1151a (deploy to stagine environment updated)
+ 1fedac1 (ignored files)
 =======
 =======
 deploy to staging
->>>>>>> abb3376 (deploy to the staging environment)
->>>>>>> db991d1 (move to deployment4)
+ abb3376 (deploy to the staging environment)
+ db991d1 (move to deployment4)
